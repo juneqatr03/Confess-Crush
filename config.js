844b8a21a99a87960875c12067d4c08a31de97eb
juneqatr03 <96308260+juneqatr03@users.noreply.github.com>
2021-12-23 19:48:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     question: 'Ê hôm nay giáng sinh, m muốn quà gì ko',
     btnReply: 'Gửi cho t >>',
     reply: 'Thôi quà cáp gì t không cần đâu nè  :> ',
-    mess: 'Hahaha m ko cần thì thôi😛😛 à mà nhớ giữ lời(NOV 3 22:59), t nhớ dai lắm, ko muoons thanh bn nh dau m m c n y m n c v t, k c m c c chán lắm:(((((((',
+    mess: 'Hahaha m ko cần thì thôi :v  à mà nhớ giữ lời(NOV 3 22:59), t nhớ dai lắm, m m c n y m n c v t, k c m c c chán lắm:(((((((',
     messDesc: 'Bữa sau thi học kì nhớ chỉ bài t nha, à khi nào chán thì vô cày lượt nghe cho t với :> tks u',
     btnAccept: 'Okiiiii lun ',
     messLink: 'https://wesingapp.com/user/609c9e83222e348e3547?lang=vi&from_page=fromugc' //link mess của các bạn. VD: https://m.me/nam.nodemy
