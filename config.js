94259eb2284d66c1cac28f1f826791300f0fa14c
juneqatr03 <96308260+juneqatr03@users.noreply.github.com>
2021-12-23 11:30:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introTitle: 'Ê đồ khùng khoa',
     introDesc: `Bấm vô đây`,
     btnIntro: '^^HiHi^^',
-    title: 'M có ghét t không',
-    desc: 'm thấy t hamlon lắm chứ gì ',
+    title: 'Bữa sau thi học kì nhớ chỉ bài t nha',
+    desc: 'Nha nha nha',
     btnYes: 'Không nha :3',
-    btnNo: 'Kó',
-    question: 'Tại sao ghét t <3',
+    btnNo: 'Oki luôn',
+    question: 'Ê hôm nay giáng sinh, m muốn quà gì ko',
     btnReply: 'Gửi cho t <3',
-    reply: 'OK đồ khùng',
-    mess: 'M ghét t t ám m hoài luôn giáng sinh chúc m fa maiz maiz hahahaa',
+    reply: 'OK, bữa sau đi học off t gửi cho',
+    mess: 'Giáng sinh cầu mong cho m  fa hoài luôn',
     messDesc: 'Bữa sau thi học kì nhớ chỉ bài t nha',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/juneqatr03' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://wesingapp.com/user/609c9e83222e348e3547/song/bMvMrNCbpGFwvCWu-CuHn?lang=vi&g_f=personal&from_page=frommv' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
